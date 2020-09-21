323b3fc0ddef6f1b063db805fb574f19aa3a14d0
Arjun Khurana <arjunkh99@gmail.com>
2020-09-21 08:38:32 -0500

    boilerplate cpu and args checking

--- cpu.h
+++ cpu.h
@@ -1,4 +1,31 @@
+using namespace std;
+
 class cpu
 {
-    
-}
+    public:
+    int AC;
+    int PC;
+    int SP;
+    int IR;
+    int X;
+    int Y;
+
+    cpu()
+    {
+        AC = 0;
+        PC = 0;
+        SP = 0;
+        IR = 0;
+        X = 0;
+        Y = 0;
+        cout << "hello i am cpu" << endl;
+    }
+
+    int execute(instr, mem)
+    {
+        switch(instr)
+        {
+            
+        }
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,4 +1,42 @@
-int main()
+#include <iostream>
+#include <fstream>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <string.h> 
+#include <sys/wait.h> 
+#include "cpu.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
 {
-    return 0;
+    int cpu_mem[2];
+    int mem_cpu[2];
+    pid_t pid;
+
+    switch(argc)
+    {
+        case 0:
+        case 1:
+        {
+            cout << "No input file detected" << endl;
+            return 1;
+        } break;
+        case 2:
+        {
+            cout << "No timer detected" << endl;
+            return 1;
+        } break;
+        case 3:
+        {
+
+        } break;
+        default:
+        {
+            cout << "Too many arguments" << endl;
+            return 1;
+        }
+    }
 }
